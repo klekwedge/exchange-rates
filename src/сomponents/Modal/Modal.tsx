4c4f75cb457e87f2d29381ffda2650a607780a74
Andrey <antonov.andrey.10101@gmail.com>
2023-08-14 22:31:53 +0400

    feat: add Modal

---
+++ src/сomponents/Modal/Modal.tsx
@@ -0,0 +1,92 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useEffect, useState } from 'react';
+
+import style from '../Modal/Modal.module.css';
+
+function Modal({ active, setActive, id }: any) {
+  const [modalContentData, setModalContentData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [formData, setFormData] = useState('');
+
+  const closeModal = (e: any) => {
+    e.currentTarget === e.target && setActive(false);
+  };
+
+  function handleSubmit(event: any) {
+    const date = new Date();
+    event.preventDefault();
+
+    fetch(`${'https://'}/${id}/comments`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/JSON',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .then(
+        setModalContentData({
+          ...modalContentData,
+          comments: [...modalContentData.comments, { id: null, text: formData.comment, date }],
+        }),
+      );
+  }
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API_URL}/${id}`)
+      .then((response) => response.json())
+      .then((data) => setModalContentData(data))
+      .then(() => setIsLoading(false));
+  }, [id]);
+
+  const handleChangeField = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  if (!modalContentData) {
+    return null;
+  }
+
+  return (
+    <div onClick={(e) => closeModal(e)} className={active ? style.modalActive : style.modal}>
+      <div className={style.modalContent}>
+        <div className={style.modalContentImage}>
+          {isLoading ? 'Loading' : <img src={modalContentData.url} alt="Gallery item" />}
+        </div>
+        <div className={style.comments}>
+          {isLoading
+            ? 'Loading'
+            : modalContentData.comments.map((comment) => (
+                <div key={comment.id} className={style.commentBody}>
+                  {/* <p className={style.commentData}>{dateFormatter(comment.date)}</p> */}
+                  <p className={style.commentText}>{comment.text}</p>
+                </div>
+              ))}
+        </div>
+        <div className={style.modalContentForm}>
+          <form>
+            <input placeholder="Ваше имя" name="name" value={formData.name} onChange={handleChangeField} />
+            <input placeholder="Ваш комментарий" name="comment" value={formData.comment} onChange={handleChangeField} />
+
+            <button type="button" onClick={handleSubmit}>
+              Оставить комментарий
+            </button>
+          </form>
+        </div>
+        <button type="button" className={style.closeButton} onClick={() => setActive(false)} />
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
