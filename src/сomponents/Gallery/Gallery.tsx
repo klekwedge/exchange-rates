e6db8bce3853ab4df955af504c69cedffdf28d2f
Andrey <antonov.andrey.10101@gmail.com>
2023-08-16 17:27:14 +0400

    feat: add Spinner

--- src/сomponents/Gallery/Gallery.tsx
+++ src/сomponents/Gallery/Gallery.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import { IImage } from '../../types';
+import Spinner from '../Spinner/Spinner';
 import styles from './Gallery.module.scss';
-
 
 interface GalleryProps {
   setActive: (value: boolean) => void;
@@ -9,30 +9,28 @@ interface GalleryProps {
   imageList: IImage[];
 }
 
-function Gallery({setActive, setId, imageList}: GalleryProps) {
-
+function Gallery({ setActive, setId, imageList }: GalleryProps) {
   const onClickSetImageList = (id: string) => {
     setActive(true);
     setId(id);
   };
 
-
   if (!imageList.length) {
-    return <h1>Loading data, please wait</h1>;
+    return <Spinner />;
   }
 
   return (
-      <div className={styles.photoWrapper}>
-        {imageList.map((photo) => (
-          <img
-            key={photo.id}
-            src={photo.urls.small}
-            alt={photo.alt_description}
-            className={styles.image}
-            onClick={() => onClickSetImageList(photo.id)}
-          />
-        ))}
-      </div>
+    <div className={styles.photoWrapper}>
+      {imageList.map((photo) => (
+        <img
+          key={photo.id}
+          src={photo.urls.small}
+          alt={photo.alt_description}
+          className={styles.image}
+          onClick={() => onClickSetImageList(photo.id)}
+        />
+      ))}
+    </div>
   );
 }
 
---
+++ src/сomponents/Spinner/Spinner.tsx
@@ -0,0 +1,11 @@
+import './Spinner.scss';
+
+function Spinner() {
+  return (
+    <div className='container'>
+      <div className="loader mx-auto" />
+    </div>
+  );
+}
+
+export default Spinner;
