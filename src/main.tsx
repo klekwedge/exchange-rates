8b8e589122197996f660b3b1fe5117cdac2e75e0
Andrey <antonov.andrey.10101@gmail.com>
2023-08-16 17:36:54 +0400

    feat: delete unused custom hook

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./сomponents/App/App";
+import App from "./components/App/App";
 import'./main.scss'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
